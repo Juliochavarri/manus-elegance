4b5fe63bfecae6ecb51526714fa8038a7de66d3a
Julio Chávarri <juliochavarrid@gmail.com>
2020-07-26 14:31:12 -0500

    carousel and modals

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('#modalVenta').click(function() {
+        $('#modal-venta').modal('show')
+    })
+
+    $('#modalContacto').click(function() {
+        $('#modal-contacto').modal('show')
+    })
+
+    $('#modalNosotros').click(function() {
+        $('#modal-nosotros').modal('show')
+    })
+})
